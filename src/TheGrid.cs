e9a90a9a55ba164eb095709768f5f9085ef500e1
HansenDJ <hansendj38@gmail.com>
2022-03-03 22:54:22 -0700

    Fixed The Grid

--- src/TheGrid.cs
+++ src/TheGrid.cs
@@ -17,13 +17,15 @@ public class TheGrid
     {
         for(int i = 0; i <= screenWidth / 5; i++)
         {
+            List<Square> tempGrid = new List<Square>();
             for(int j = 0; j <= screenHeight / 5; j++)
             {
                 Square square = new Square();
                 square.x = i * 5;
                 square.y = j * 5;
-                getGrid() [i].Add(square);
+                tempGrid.Add(square);
             }
+            grid.Add(tempGrid);
         }
     }
 }
