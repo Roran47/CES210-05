23247a10b31b82ffbc64820a4291d3e7554af70f
HansenDJ <hansendj38@gmail.com>
2022-03-07 12:41:43 -0700

    Added checkCollision method. Commented out up and down keyboard

--- src/Collision.cs
+++ src/Collision.cs
@@ -4,5 +4,11 @@ namespace generalNamespace;
 
 public class Collision
 {
-    
+    public bool checkCollision(int playerX, int playerY)
+    {
+        int squareX = playerX / 5;
+        int squareY = playerY / 5;
+
+        
+    }
 }
