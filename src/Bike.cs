142ed54f4bf9b1ff67dadb7275a454a6a0dabe5d
dosmaster4life <jefferysuperman@gmail.com>
2022-03-07 12:04:55 -0700

    Bike Class

--- src/Bike.cs
+++ src/Bike.cs
@@ -1,8 +1,19 @@
 using System;
+using Raylib_cs;
+using static Raylib_cs.Color;
 
 namespace generalNamespace;
 
 public class Bike
 {
-    
+    public int bikeX;
+    public int bikeY;
+    public int bikeWidth = 5;
+    public int bikeHeight = 5;
+    public Color bikeColor = BLUE;
+    public void changeColor(Color color)
+    {
+       bikeColor = color;
+    }
+
 }
