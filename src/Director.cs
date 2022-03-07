6d25cd5f4b9098f6dff39893bd82b54720379d82
dosmaster4life <jefferysuperman@gmail.com>
2022-03-07 12:17:53 -0700

    Timer and Player initiated

--- src/Director.cs
+++ src/Director.cs
@@ -2,19 +2,43 @@ using System;
 using System.Timers;
 using Raylib_cs;
 namespace generalNamespace;
+using System.Timers;
+using static Raylib_cs.Color;
 
 public class Director
 {
+       private static System.Timers.Timer timer;
+     static bool action = false;
      public void startGame()
     {
         TheGrid theGrid = new TheGrid();
         theGrid.makeGrid();
+        Bike player1 = new Bike();
+        Bike player2 = new Bike();
+        player1.changeColor(BLUE);
+         player2.changeColor(RED);
         var vd = new VideoService();
         Raylib.InitWindow(1000, 1000, "norT");
          while (!Raylib.WindowShouldClose())
         {
-            vd.draw(theGrid.getGrid());
+            if(action) {
+                  vd.draw(theGrid.getGrid());
+                
+                  action = false;
+            }
+          
         }
         
     }
+     static void SetTimer() {
+            timer = new System.Timers.Timer(20);
+
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        static void OnTimedEvent(Object source, ElapsedEventArgs e) {
+            action = true;
+        }
 }
