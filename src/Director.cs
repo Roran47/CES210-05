053f068ca71a9137205974338301c9a2bf771e78
dosmaster4life <jefferysuperman@gmail.com>
2022-03-09 12:29:42 -0700

    timer dispose on gameover

--- src/Director.cs
+++ src/Director.cs
@@ -31,8 +31,15 @@ public class Director
                   vd.draw(theGrid.getGrid());
                   vd.drawBike(player1);
                   vd.drawBike(player2);
-                  Collision.checkCollision(player1,theGrid.getGrid());
-                  Collision.checkCollision(player2,theGrid.getGrid());
+                  if(Collision.checkCollision(player1,theGrid.getGrid())) {
+                      timer.Dispose();
+                      // Call Gameover Method
+                  }
+                   if(  Collision.checkCollision(player2,theGrid.getGrid())) {
+                       timer.Dispose();
+                       // Call Gameover Method
+                   }
+                
                   action = false;
                   Raylib.EndDrawing();
             }
