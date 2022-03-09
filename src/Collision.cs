fe809899442243e87c12dc8e4a033946fb2e5aac
Roran47 <ekcook@live.com>
2022-03-09 12:27:36 -0800

    colistion completed.

--- src/Collision.cs
+++ src/Collision.cs
@@ -1,15 +1,21 @@
 using System;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
 
 namespace generalNamespace;
 
 public static class Collision
 {
-    public static bool checkCollision(Bike player, TheGrid thegrid)
+    public static bool checkCollision(Bike player, List<List<Square>> thegrid)
     {
         int squareX = player.bikeX / 5;
         int squareY = player.bikeY / 5;
-
-        return false;
-        
+        if(thegrid[squareX][squareY].squareColor.Equals(BLACK)){
+            return false;
+            
+        }else{
+            return true;
+        }
     }
 }
